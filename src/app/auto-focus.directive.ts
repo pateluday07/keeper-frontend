36ee0806604e6a64154d762f867a517361c83876
pateluday07 <pateluday07@gmail.com>
2022-01-03 23:48:05 +0530

    create custom auto-focus directive

---
+++ src/app/auto-focus.directive.ts
@@ -0,0 +1,14 @@
+import { AfterContentInit, Directive, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appAutoFocus]'
+})
+export class AutoFocusDirective implements AfterContentInit {
+
+  constructor(private el: ElementRef) { }
+
+  ngAfterContentInit(): void {
+    this.el.nativeElement.focus();
+  }
+
+}
